8dd7e077495ac884de5fce2913deee6d8caaa274
Agacia <agacia@gmail.com>
2017-07-30 15:30:58 +0200

    New JS file

---
+++ JS/scripts.js
@@ -0,0 +1,20 @@
+var a;
+var b;
+var value = (a * a) + (2 * a * b) - (b * b);
+console.log(value);
+
+if (value > 0) {
+    console.log("Wynik dodatni");
+} 
+else if (value === 0) {
+    console.log("Wynik równy 0");
+}
+else
+{
+    console.log("Wynik ujemny");
+}
+
+
+
+
+
